17eca2362630153a74131e9b707a17427e200bc8
Шацких Павел Павлович <Shatskikh.P.Pa@sberbank.ru>
2022-01-03 07:44:18 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("Ok")
 }

2cd2394f070c12f097f01aa35e37478293fd5d3b
pavel.shackih@gmail.com <pavel.shackih@gmail.com>
2022-01-04 15:03:03 +0300

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::io::Result;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
         .bind("127.0.0.1:8080")?
         .run()
